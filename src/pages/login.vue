7c60ab202520f4e3faec9e4d6552aa55e32fece4
ecaf1 <edeilsonazevedo22@gmail.com>
2024-09-17 15:23:16 -0300

    add login page

---
+++ src/pages/login.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <h1 class="font-weight-bold text-3xl text-center">FinanSys</h1>
+
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
+    >
+      <div class="text-subtitle-1 text-medium-emphasis">Conta</div>
+
+      <v-text-field
+        density="compact"
+        placeholder="Endereço de email"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Senha
+
+        <a
+          class="text-caption text-decoration-none text-blue"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Esqueceu a senha?</a>
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Digite sua senha"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <v-btn
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+        block
+        @click="submitForm"
+      >
+        Entrar
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <a
+          class="text-blue text-decoration-none"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Cadastre-se agora <v-icon icon="mdi-chevron-right"></v-icon>
+        </a>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      visible: false,
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$router.push('/new');
+    },
+  },
+}
+</script>
