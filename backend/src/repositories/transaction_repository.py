0c0bab8426032b019bb15c04d8d55dbb07725915
ecaf1 <edeilsonazevedo22@gmail.com>
2024-11-12 18:05:56 -0300

    chore: update transaction repository

--- backend/src/repositories/transaction_repository.py
+++ backend/src/repositories/transaction_repository.py
@@ -1,14 +1,17 @@
-from typing import Annotated
+from typing import List, Tuple, Annotated
 from fastapi import Depends
-from sqlmodel import Session, select
+from sqlmodel import Session, select, func
 from src.models.transaction_model import Transaction
 from src.common.db import engine
 
-class ITransactionRepository():
+class ITransactionRepository:
     def add(transaction: Transaction) -> None:
         raise NotImplementedError
     
-    def list(user_id) -> list[Transaction]:
+    def list(user_id: int) -> List[Transaction]:
+        raise NotImplementedError
+    
+    def get_total_and_category_expenses(user_id: int) -> Tuple[float, List[Tuple[str, float]]]:
         raise NotImplementedError
 
 class TransactionRepository(ITransactionRepository):
@@ -17,11 +20,24 @@ class TransactionRepository(ITransactionRepository):
             session.add(transaction)
             session.commit()
             
-    def list(self, user_id: int) -> list[Transaction]:
+    def list(self, user_id: int) -> List[Transaction]:
         with Session(engine) as session:
             sttm = select(Transaction).where(Transaction.user_id == user_id)
             data = session.exec(sttm).all()
-            
             return data
+    
+    def get_total_and_category_expenses(self, user_id: int) -> Tuple[float, List[Tuple[str, float]]]:
+        with Session(engine) as session:
+            total_stmt = select(func.sum(Transaction.value)).where(Transaction.user_id == user_id)
+            total = session.exec(total_stmt).one() or 0.0
+
+            category_stmt = (
+                select(Transaction.category, func.sum(Transaction.value))
+                .where(Transaction.user_id == user_id)
+                .group_by(Transaction.category)
+            )
+            category_expenses = session.exec(category_stmt).all()
             
+            return total, category_expenses
+
 TransactionRepositoryDep = Annotated[ITransactionRepository, Depends(TransactionRepository)]
