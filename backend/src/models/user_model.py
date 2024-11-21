bcc05db2b04fb146765451535cdfb0c81e1ee63f
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-11-20 23:31:56 -0300

    fix: user password type

--- backend/src/models/user_model.py
+++ backend/src/models/user_model.py
@@ -5,7 +5,7 @@ class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str 
     email: str = Field(unique=True)
-    password: str
+    password: bytes
     
     transactions: list["Transaction"] = Relationship(back_populates="user") # type: ignore
     
