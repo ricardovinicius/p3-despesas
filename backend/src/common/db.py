16dd66623cfa3fd0044dcff254777e52c780465a
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-11-20 23:01:08 -0300

    fix: add external db

--- backend/src/common/db.py
+++ backend/src/common/db.py
@@ -3,13 +3,14 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session
 
+from .settings import get_settings
+
 from ..models import user_model, transaction_model
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+settings = get_settings()
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(settings.database_url)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
--- backend/src/common/settings.py
+++ backend/src/common/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     jwt_secret_key: str = "JWT_SECRET_KEY"
+    database_url: str = f"sqlite:///database.db"
     
     model_config = SettingsConfigDict(env_file=".env")
 
