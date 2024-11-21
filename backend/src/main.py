467a80354bb389028ed1a2fb164dd24e4fc9de82
ecaf1 <edeilsonazevedo22@gmail.com>
2024-11-21 13:30:37 -0300

    feat: add goal router to main application

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.routers import transaction
+from src.routers import transaction, goal
 
 from .common.db import create_db_and_tables, SessionDep
 from .routers import main, user
@@ -24,6 +24,7 @@ app.add_middleware(
 app.include_router(main.router)
 app.include_router(user.router)
 app.include_router(transaction.router)
+app.include_router(goal.router)
 
 @app.on_event("startup")
 def on_startup():
